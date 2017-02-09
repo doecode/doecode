64704a2021c69d1c9aba1ceb0c8316d45e7e36d1
nensor <nensor@gmail.com>
2017-02-09 08:51:20 -0500

    modified to return JSON properly, logging added Signed-off-by: nensor <nensor@gmail.com>

--- demo/react-demo/src/main/java/gov/osti/handlers/ReactHandler.java
+++ demo/react-demo/src/main/java/gov/osti/handlers/ReactHandler.java
@@ -7,13 +7,17 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.LoggerFactory;
 
 
 import gov.osti.connectors.Connector;
 import gov.osti.entity.DOECodeMetadata;
+import org.slf4j.Logger;
 
 public class ReactHandler {
-
+    // logger
+    private static final Logger log = LoggerFactory.getLogger(ReactHandler.class);
+    // Jackson object mapper instance
     private static final ObjectMapper mapper = new ObjectMapper();
 
 	public static String handleRequest(HttpServletRequest request) throws IOException {
@@ -65,6 +69,7 @@ public class ReactHandler {
 	private static String handleActionSave(BufferedReader reader) throws IOException {
             DOECodeMetadata md = DOECodeMetadata.parseJson(reader);
             md.save();
-            return mapper.createObjectNode().putPOJO("metadata", md).toString();
+            log.info("JSON: " + md.toJson());
+            return mapper.createObjectNode().putPOJO("metadata", md.toJson()).toString();
 	}
 }
